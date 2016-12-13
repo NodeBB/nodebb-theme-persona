41d7ec3c0c9a613b3fb2c2e66a13dd30b4e19e36
pichalite <pichalite.com@gmail.com>
2016-12-13 22:04:15 +0000

    Quick reply option

--- library.js
+++ library.js
@@ -2,6 +2,7 @@
 
 var S = require.main.require('string');
 var	meta = module.parent.require('./meta');
+var user = module.parent.require('./user');
 
 var library = {};
 
@@ -91,6 +92,7 @@ library.getThemeConfig = function(config, callback) {
 	meta.settings.get('persona', function(err, settings) {
 		config.hideSubCategories = settings.hideSubCategories === 'on';
 		config.hideCategoryLastPost = settings.hideCategoryLastPost === 'on';
+		config.enableQuickReply = settings.enableQuickReply === 'on';
 	});
 
 	callback(false, config);
@@ -99,5 +101,20 @@ library.getThemeConfig = function(config, callback) {
 function renderAdmin(req, res, next) {
 	res.render('admin/plugins/persona', {});
 }
+
+library.addUserToTopic = function(data, callback) {
+	if (data.req.user) {
+		user.getUserData(data.req.user.uid, function(err, userdata) {
+			if (err) {
+				return callback(err);
+			}
+			
+			data.templateData.loggedInUser = userdata;
+			callback(null, data);
+		});
+	} else {
+		callback(null, data);
+	}
+};
 
 module.exports = library;
