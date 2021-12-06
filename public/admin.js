3db51e506460806347ee0db162c59648d0d242fd
Barış Soner Uşaklı <barisusakli@gmail.com>
2021-12-06 12:52:41 -0500

    refactor: dont use app.alert

--- public/admin.js
+++ public/admin.js
@@ -1,22 +1,13 @@
 'use strict';
-/* globals $, app */
 
-define('admin/plugins/persona', ['settings'], function(Settings) {
-
+define('admin/plugins/persona', ['settings'], function (Settings) {
 	var ACP = {};
 
-	ACP.init = function() {
+	ACP.init = function () {
 		Settings.load('persona', $('.persona-settings'));
 
-		$('#save').on('click', function() {
-			Settings.save('persona', $('.persona-settings'), function() {
-				app.alert({
-					type: 'success',
-					alert_id: 'persona-saved',
-					title: 'Settings Saved',
-					message: 'Persona settings saved'
-				});
-			});
+		$('#save').on('click', function () {
+			Settings.save('persona', $('.persona-settings'));
 		});
 	};
 
